c8d9816089ccb3d1a5f52ab4aa8219beb0b245d9
EC2 Default User <ec2-user@ip-172-31-3-199.ap-northeast-1.compute.internal>
2022-09-19 15:55:32 +0000

    [Add]エラー苦戦してます

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,28 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
-
+  
+  def create
+     book = Book.new(book_params)
+     book.save
+     redirect_to "/books"
+  end
+  
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
   end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+  
 end

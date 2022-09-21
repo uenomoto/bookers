d9a8cb3a8cf13f6f06f86df54f5edf05dab8d096
EC2 Default User <ec2-user@ip-172-31-3-199.ap-northeast-1.compute.internal>
2022-09-21 15:03:57 +0000

    [Add]またエラー苦戦してます

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,8 +25,11 @@ class BooksController < ApplicationController
   
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
+    if book.update(book_params)
     redirect_to book_path(book.id)
+    else
+    render :new
+    end
   end
   
   def destroy

32e6b2ad3c50d8ba8254c89e541c403c309ef78d
EC2 Default User <ec2-user@ip-172-31-10-103.ap-northeast-1.compute.internal>
2023-02-03 08:31:31 +0000

    定時処理投稿削除実行しました今は停止中です

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module Bookers
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.paths.add 'lib', eager_load: true
   end
 end
